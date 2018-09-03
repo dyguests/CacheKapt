798b22bf98023c93353e4df450c4808b3a774f37
fanhl <fanhonglin@klicen.com>
2018-09-03 11:35:16 +0800

    counter

--- sample/src/main/java/com/fanhl/cachekapt/sample/MainActivity.kt
+++ sample/src/main/java/com/fanhl/cachekapt/sample/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.fanhl.cachekapt.sample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
+
+
+    private var count = 0
+        get() {
+            field++
+            return field
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toast("counter:$count")
     }
 }
