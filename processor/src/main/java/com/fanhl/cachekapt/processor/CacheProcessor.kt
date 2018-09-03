a32495702565b5974777f43655c6d2d16dd1f833
fanhl <fanhonglin@klicen.com>
2018-09-03 13:54:46 +0800

    FileSpec

--- processor/src/main/java/com/fanhl/cachekapt/processor/CacheProcessor.kt
+++ processor/src/main/java/com/fanhl/cachekapt/processor/CacheProcessor.kt
@@ -2,6 +2,10 @@ package com.fanhl.cachekapt.processor
 
 import com.fanhl.cachekapt.annotation.Cache
 import com.google.auto.service.AutoService
+import com.squareup.kotlinpoet.FileSpec
+import com.squareup.kotlinpoet.FunSpec
+import com.squareup.kotlinpoet.TypeSpec
+import java.io.File
 import java.io.IOException
 import javax.annotation.processing.*
 import javax.lang.model.SourceVersion
@@ -15,15 +19,32 @@ import javax.lang.model.element.TypeElement
 @SupportedOptions(CacheProcessor.KAPT_KOTLIN_GENERATED_OPTION_NAME)
 class CacheProcessor : AbstractProcessor() {
     override fun process(set: MutableSet<out TypeElement>?, roundEnv: RoundEnvironment?): Boolean {
+        roundEnv?.getElementsAnnotatedWith(Cache::class.java)?.forEach {
+            val fileName = "Cache_Test1"
+            val file = FileSpec.builder("com.fanhl.pack1", fileName)
+                .addType(
+                    TypeSpec.classBuilder(fileName)
+                        .addFunction(
+                            FunSpec.builder("getName")
+                                .addStatement("return \"World\"")
+                                .build()
+                        )
+                        .build()
+                )
+                .build()
 
-        createGeneratedClass(roundEnv)
+            val kaptKotlinGeneratedDir = processingEnv.options[KAPT_KOTLIN_GENERATED_OPTION_NAME]
+            file.writeTo(File(kaptKotlinGeneratedDir, "$fileName.kt"))
+        }
 
         return true
     }
 
     override fun getSupportedAnnotationTypes() = mutableSetOf(Cache::class.java.canonicalName)
 
-
+    /**
+     * 这只是用来测试的方法
+     */
     private fun createGeneratedClass(roundEnv: RoundEnvironment?) {
         //last
         val builder = StringBuilder()
